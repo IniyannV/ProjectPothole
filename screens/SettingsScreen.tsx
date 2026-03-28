fec6b6fdd42b4873768a4337726c1ea3fa06c1f5
Soumil Voona <voonasoumil@gmail.com>
2026-03-28 09:25:54 -0500

    navbar

---
+++ screens/SettingsScreen.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+function SettingsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Settings</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+  },
+});
+
+export default SettingsScreen;
